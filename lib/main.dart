d3409b518abd2292312565b72b6e33852d878d71
Harshiikaa <chaudharyharshika8@gmail.com>
2023-06-10 17:19:19 +0545

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../../../Push garna/intruderdetection/lib/Screens/biometrics_login.dart';
+import 'package:intruderdetection/Screens/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Fingerprint(),
+      home: Dashboard(),
     );
   }
 }
